214841afe5bcd20df051309cc2e42efe0e0bab5f
Jeff Hollan <jeffhollan@outlook.com>
2016-02-29 16:32:20 -0800

    added Twilio metadata

--- URLEncodedtoJsonAPI/Controllers/UrlEncodedToJsonController.cs
+++ URLEncodedtoJsonAPI/Controllers/UrlEncodedToJsonController.cs
@@ -1,29 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Web.Http;
-using System.Text;
-using System.Globalization;
-using System.Net;
 using System.Web;
-using Newtonsoft.Json;
-using System.Web.Script.Serialization;
-using System.Collections.Specialized;
 
 namespace URLEncodedtoJsonAPI.Controllers
 {
     public class UrlEncodedToJsonController : ApiController
     {
-        public HttpResponseMessage UrlEncodedToJson([FromUri] string urlEncodedString)
+        [Swashbuckle.Swagger.Annotations.SwaggerResponse(System.Net.HttpStatusCode.OK, "Twilio Message", typeof(TwilioResponse))]
+        [HttpPost]
+        public async System.Threading.Tasks.Task<HttpResponseMessage> UrlEncodedToJson(TwilioResponse twil)
         {
-            var formData = HttpUtility.ParseQueryString(urlEncodedString);
-            var dict = new Dictionary<string, string>();
-            foreach(string key in formData)
-            {
-                dict.Add(key, formData[key]);
-            }
-            return Request.CreateResponse(dict);
+            //var response = await urlEncodedContent.ReadAsAsync<TwilioResponse>();
+            return Request.CreateResponse(twil);
         }
+    }
+
+    public class TwilioResponse
+    {
+        public string ToCountry { get; set; }
+        public string ToState { get; set; }
+        public string SmsMessageSid { get; set; }
+        public string NumMedia { get; set; }
+        public string ToCity { get; set; }
+        public string FromZip { get; set; }
+        public string SmsSid { get; set; }
+        public string FromState { get; set; }
+        public string SmsStatus { get; set; }
+        public string FromCity { get; set; }
+        public string Body { get; set; }
+        public string FromCountry { get; set; }
+        public string To { get; set; }
+        public string ToZip { get; set; }
+        public string NumSegments { get; set; }
+        public string MessageSid { get; set; }
+        public string AccountSid { get; set; }
+        public string From { get; set; }
+        public string ApiVersion { get; set; }
     }
 }
 
